d930d2e9686aa9a98fe90cc7e455b3f46ea4169b
Artem Shabalin <tomshabalin95@gmail.com>
2023-04-30 17:49:26 +0300

    maps

--- 06/map/App.js
+++ 06/map/App.js
@@ -1,11 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import { StyleSheet, View, Text } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function App() {
+  const [pin, setPin] = useState({
+    latitude: 32.103376857642246,
+    longitude: 35.20905301042528,
+  });
+
+  const [region, setRegion] = useState({
+    latitude: 32.103376857642246,
+    longitude: 35.20905301042528,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 50, flex: 1 }}>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        GooglePlacesSearchQuery={{
+          rankby: "distance",
+        }}
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          console.log(data, details);
+          setRegion({
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        }}
+        query={{
+          key: "AIzaSyA7bj1RIYNsklS7GwFMfzYuaeS-MyZwZU0",
+          language: "en",
+          // components: "country: us",
+          types: "establishment",
+          radius: 30000,
+          location: `${region.latitude},${region.longitude}`,
+        }}
+        styles={{
+          container: {
+            flex: 0,
+            position: "absolute",
+            width: "100%",
+            zIndex: 1,
+          },
+          listView: { backgroundColor: "white" },
+        }}
+      />
+      <MapView
+        style={styles.map}
+        // 32.103376857642246, 35.20905301042528
+        initialRegion={{
+          latitude: 32.103376857642246,
+          longitude: 35.20905301042528,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google" // for google maps insted of default map of the phone
+      >
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+        />
+        <Marker
+          coordinate={pin}
+          pinColor="red"
+          draggable={true}
+          onDragStart={(event) => {
+            console.log("Drag start", event.nativeEvent.coordinate);
+          }}
+          onDragEnd={(event) => {
+            setPin({
+              latitude: event.nativeEvent.coordinate.latitude,
+              longitude: event.nativeEvent.coordinate.longitude,
+            });
+          }}
+        >
+          <Callout>
+            <Text>I'm Here</Text>
+          </Callout>
+        </Marker>
+        <Circle center={pin} radius={500}></Circle>
+      </MapView>
     </View>
   );
 }
@@ -13,8 +95,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ 06/map/Installation.md
@@ -0,0 +1,15 @@
+> https://docs.expo.dev/versions/latest/sdk/map-view/
+
+```bash
+npm install react-native-maps
+```
+
+### React native maps repository with all the components and API's
+
+> https://github.com/react-native-maps/react-native-maps
+
+```bash
+npm install react-native-google-places-autocomplete --save
+```
+
+### Then create a google console cloud account to enable the Google API's (it's free)
